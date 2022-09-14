366cabeff97697c15c05feea13d0f94e9b160067
etafoh <33801223+etafoh@users.noreply.github.com>
2022-09-14 03:47:24 +0100

    Update c1-versions.tf

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -1,18 +1,18 @@
 # Terraform Block
 terraform {
-  required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = "1.2.9" 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "=> 3.0"
     }
     null = {
       source = "hashicorp/null"
-      version = "~> 3.0"
+      version = "=> 3.0"
     }    
     random = {
       source = "hashicorp/random"
-      version = "~> 3.0"
+      version = "=> 3.0"
     }            
   }
   # Adding Backend as S3 for Remote State Storage
