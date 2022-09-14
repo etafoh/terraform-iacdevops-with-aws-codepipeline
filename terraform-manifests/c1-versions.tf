45b2c5ac6a3f26864d3d194694f5dd65e335134d
etafoh <33801223+etafoh@users.noreply.github.com>
2022-09-14 04:35:30 +0100

    Update c1-versions.tf

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -21,8 +21,7 @@ terraform {
 
 # Provider Block
 provider "aws" {
-  region  = var.aws_region
-  #profile = "default"
+  region  = var.aws_region 
 }
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
